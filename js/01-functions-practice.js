81dce21aeff28e0628dbca3192cc7da7e2d9032f
Hippieonholiday <cassidybrynn@gmail.com>
2024-10-18 07:06:35 -0800

    inital

---
+++ js/01-functions-practice.js
@@ -0,0 +1,24 @@
+//STEP 1
+
+
+var num = parsefloat(prompt: 'Enter a number');
+const sum = halfNumber(num, 2); {
+    return num / 2;
+}
+sum()
+
+
+
+alert: parseFloat('Enter a number')
+const sum = halfNumber(5, 2) {
+    return: (5 / 2)
+}
+Console.log(sum)
+
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
